83b3d89342ea0672114d43742f1b05f0c6d79740
marlass <lasakmarcin90@gmail.com>
2016-01-18 16:28:27 +0100

    Basic server.js implementation

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Node.js\n');
-}).listen(8124, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:8124/');
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 8080;
+
+const router = express.Router();
+
+router.get('/', function(req, res) {
+   res.json({ message: "Hi. Welcome to our API!" }); 
+});
+
+app.use('/api', router);
+
+app.listen(port);
+console.log('API works on port ' + port);
