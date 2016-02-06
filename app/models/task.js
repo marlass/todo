49478531275c96288b25c8436920383d9c58640f
Marcin Lasak <lasakmarcin90@gmail.com>
2016-02-06 14:47:20 +0100

    Fix paths

--- app/models/task.js
+++ app/models/task.js
@@ -1,3 +1,5 @@
+const Project = require('./../models/project');
+
 const mongoose     = require('mongoose');
 const Schema       = mongoose.Schema;
 const ProjectSchema = require('mongoose').model('Project').schema
